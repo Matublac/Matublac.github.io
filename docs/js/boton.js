32ef97e05ca2d0ae1431f55e1fafdb8c4862c283
Matias Blacher <matias.a.blacher@gmail.com>
2023-02-18 13:33:25 -0300

    web

---
+++ docs/js/boton.js
@@ -0,0 +1,41 @@
+
+          function estudioscursados()
+          { window.location.assign ("./estudioscursados.html")}
+
+          function experiencialaboral()
+          { window.location.assign ("./explaboral.html")}
+
+          function villanos()
+          { window.location.assign ("./villanos.html")}
+
+          function volverinicio()
+          { window.location.assign ("./index.html")}
+
+          function video()
+          { window.location.assign ("./Video.html")}
+
+          function contacto()
+          { window.location.assign ("./contacto.html")}
+
+       
+
+         function enviar()
+          
+          {
+            resultado = window.confirm ("¿Estás seguro que deseas activar la batiseñal?")
+           
+           if (resultado===true)
+            { 
+            window.alert ("La batiseñal ha sido activada con éxito")
+            window.location.assign ("./index.html")
+
+                        
+          } else  {  
+                window.location.assign ("./contacto.html")
+            } 
+           
+          }
+
+ 
+
+
